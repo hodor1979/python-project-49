d3e3be3adfd04162e7645c407a978547e7acdf39
hodor1979 <alexander.khodakov1979@gmail.com>
2023-02-09 18:20:26 +1100

    brain_calc.py spelling fixed again

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -5,7 +5,7 @@ from brain_games.games import brain_calc_logic
 
 def main():
     # brain_games.welcome_user()
-    intro = 'What it the result of the expression?'
+    intro = 'What is the result of the expression?'
     any_game_logic.main(brain_calc_logic.calc_logic, intro)
 
 
