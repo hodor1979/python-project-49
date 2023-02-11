e7c3b67aae33664684bce0300b9cb0bc1fdff7e3
hodor1979 <alexander.khodakov1979@gmail.com>
2023-02-11 13:56:33 +1100

    somethong fixed

--- brain_games/games/brain_prime_logic.py
+++ brain_games/games/brain_prime_logic.py
@@ -2,14 +2,16 @@ from random import randint
 
 
 def prime_logic():  # логика только для игры в простые числа
-    question = randint(1, 51)
-    count = 0
-    for i in range(1, question):
-        if question % i == 0:
-            count += 1
-            break
-    if count > 0:
-        correct_answer = "no"
-    else:
-        correct_answer = "yes"
+    for i in range(1,51):
+        question = randint(1, 30)
+        count = 0
+        for i in range(1, question):
+            if question % i == 0:
+                count += 1
+            if count > 1:
+                break
+        if count > 1:
+            correct_answer = "no"
+        else:
+            correct_answer = "yes"
     return question, correct_answer
