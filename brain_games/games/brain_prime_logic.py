6a8822d357e78f2e50ae22630e91bf242b8cff63
hodor1979 <alexander.khodakov1979@gmail.com>
2023-02-11 12:46:52 +1100

    brake returned toq if-block

--- brain_games/games/brain_prime_logic.py
+++ brain_games/games/brain_prime_logic.py
@@ -2,13 +2,14 @@ from random import randint
 
 
 def prime_logic():  # логика только для игры в простые числа
-    question = randint(1, 51)
+    question = randint(1, 99)
     count = 0
     for i in range(1, question):
         if question % i == 0:
             count += 1
-                if count > 1:
-        correct_answer = "no"
-    else:
-        correct_answer = "yes"
+            if count > 1:
+                correct_answer = "no"
+                break
+        else:
+            correct_answer = "yes"
     return question, correct_answer
