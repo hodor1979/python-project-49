f22df07aff6682dd9543541c37e11eb53c7e81aa
hodor1979 <alexander.khodakov1979@gmail.com>
2023-02-11 11:53:43 +1100

    Codeblock "else" removed from brain_prime_logic.py

--- brain_games/games/brain_prime_logic.py
+++ brain_games/games/brain_prime_logic.py
@@ -1,14 +1,13 @@
 from random import randint
 
 
-def prime_logic():  # логика только для игры в чётные числа
-    question = randint(1, 21)
+def prime_logic():  # логика только для игры в простые числа
+    question = randint(1, 51)
     count = 0
     for i in range(1, question):
         if question % i == 0:
             count += 1
-        else:
-            continue
+            break
     if count > 1:
         correct_answer = "no"
     else:
